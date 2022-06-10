a98c0d22df31d72439b6d3bfdf62bde75ebe10c3
Mamadou Mariko <mamadoumariko@MacBook-Pro-de-Mamadou.local>
2022-06-10 17:23:20 +0200

    add method orderProposalsByScore

--- index.php
+++ index.php
@@ -98,6 +98,8 @@ function scheduleProposalsBy(array $proposals, DateTime $deadline, int $proposal
     $counter = 1;
     $counterWave = 1;
 
+    // TODO here possibility to add $proposals = orderProposalsByScore($proposals);
+
     for ($i = 1; $i <= $proposalsNumber; $i++) {
         $proposals[$i-1]->editSendAt(
             new DateTime(date('Y-m-d H:i:s', $now)),
@@ -120,4 +122,37 @@ function scheduleProposalsBy(array $proposals, DateTime $deadline, int $proposal
     return $proposals;
 }
 
+/**
+ * @param ProposalUser[] $proposals
+ * @return array
+ */
+function orderProposalsByScore(array $proposals): array
+{
+    $isPermut = true;
 
+    while ($isPermut)
+    {
+        $isPermut = false;
+
+        for ($i = 0; $i < count($proposals); $i++)
+        {
+            if (isset($proposals[$i+1])) {
+                $current = $proposals[$i];
+                $next = $proposals[$i+1];
+
+                if ($proposals[$i]->readScore() < $proposals[$i+1]->readScore())
+                {
+                    if(!$isPermut) {
+                        $isPermut = true;
+                    }
+
+                    $proposals[$i] = $next;
+                    $proposals[$i+1] = $current;
+
+                }
+            }
+        }
+    }
+
+    return $proposals;
+}
