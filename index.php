2533bb837926c781546c0e810281f4bfe09e9ba6
Mamadou Mariko <mamadoumariko@MacBook-Pro-de-Mamadou.local>
2022-06-10 14:54:26 +0200

    init

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+
+class EnsureValid {
+    public static function ensureScore(int $score): int
+    {
+        if ($score < 0 || $score > 100) {
+            throw new ErrorException('Score has an invalid format');
+        }
+
+        return $score;
+    }
+}
+
+
+class ProposalUser {
+    private int $userId;
+    private ?string $proposalMessage;
+    private ?int $score;
+    private string $type;
+    private ?DateTime $sendAt;
+
+    public function __construct(
+        int $userId,
+        string $type,
+        ?int $score,
+        ?string $proposalMessage,
+    ) {
+        $this->userId = $userId;
+        $this->proposalMessage = $proposalMessage;
+        $this->type = $type;
+        $this->score = $score ?? null;
+        $this->sendAt = null;
+    }
+
+    public function setSendAt(?DateTime $dateTime): self
+    {
+        $this->sendAt = $dateTime;
+
+        return $this;
+    }
+
+    public function getSendAt(): ?DateTime
+    {
+        return $this->sendAt;
+    }
+
+    public function getProposalMessage(): ?string
+    {
+        return $this->proposalMessage;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = EnsureValid::ensureScore($score);
+
+        return $this;
+    }
+
+    public function getScore(): int
+    {
+        return $this->score;
+    }
+
+    public function getType(string $type): string
+    {
+        return $this->type;
+    }
+}
+
+/**
+ * @param ProposalUser[] $proposals
+ * @param DateTime $deadline
+ * @param int $proposalsNumberMINByWave
+ * @return array
+ * @throws Exception
+ */
+function scheduleProposalsBy(array $proposals, DateTime $deadline, int $proposalsNumberMINByWave): array
+{
+    $proposalsNumber = count($proposals);
+
+    if($proposalsNumberMINByWave === 0)
+    {
+        $proposalsNumberMINByWave = 1;
+    }
+
+    $waveTime = 300; // 5 min
+    $now = time(); // Current TIMESTAMP UNIX
+
+    if($deadline->getTimestamp() < $now)
+    {
+        throw new ErrorException('Deadline must be greater than current time');
+    }
+
+    // we need to get waves number
+    $periodTime = $deadline->getTimestamp() - $now;
+    $wavesNumber = $periodTime / $waveTime;
+    $wavesNumber = $wavesNumber > 1 ? (int)(floor($wavesNumber)) : (int)(ceil($wavesNumber));
+
+    if($proposalsNumber < $wavesNumber)
+    {
+        $wavesNumber = $proposalsNumber;
+    }
+
+    $proposalsByWave = $proposalsNumber / $wavesNumber;
+    $proposalsByWave = $proposalsByWave > 1 ? (int)(floor($proposalsByWave)) : (int)(ceil($proposalsByWave));
+
+    // Proposals number must be greater than proposals minimal
+    if($proposalsNumberMINByWave > $proposalsByWave) {
+        $proposalsByWave = $proposalsNumberMINByWave;
+    }
+
+    $counter = 1;
+    $counterWave = 1;
+
+    for ($i = 1; $i <= $proposalsNumber; $i++) {
+        $proposals[$i-1]->setSendAt(new DateTime(date('Y-m-d H:i:s', $now)));
+
+        if ($counter === $proposalsByWave) {
+            $counter = 1;
+
+            if($counterWave < $wavesNumber) {
+                $now += 300;
+            }
+
+            $counterWave++;
+            continue;
+        }
+
+        $counter++;
+    }
+
+    return $proposals;
+}
